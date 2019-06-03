aa1276d306445c8586c0f8ca8700cdcd56c9c58f
yacovm <yacovm@il.ibm.com>
2019-06-03 14:47:04 +0300

    make protos in build_checks
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- scripts/build_checks.sh
+++ scripts/build_checks.sh
@@ -7,6 +7,7 @@ if [[ $unformatted == "" ]];then
 else
     echo "The following files needs gofmt:"
     echo "$unformatted"
+    exit 1
 fi
 
 unformatted=$(find . -name "*.go" | grep -v "^./vendor" | grep -v "pb.go" | xargs goimports -l)
@@ -16,4 +17,16 @@ if [[ $unformatted == "" ]];then
 else
     echo "The following files needs goimports:"
     echo "$unformatted"
+    exit 1
 fi
+
+make protos
+git status | grep -q "modified"
+if [ $? -eq 0 ];then
+	git status
+	echo "protobuf not up to date"
+	exit 1
+fi
+
+
+exit 0
