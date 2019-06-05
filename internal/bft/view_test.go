8f8ff373afa6c87aaf95e65158f4de602d74160d
Hagar Meir <hagar.meir@ibm.com>
2019-06-05 14:16:12 +0300

    added tests
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- internal/bft/view_test.go
+++ internal/bft/view_test.go
@@ -6,6 +6,7 @@
 package bft_test
 
 import (
+	"errors"
 	"sync"
 	"testing"
 
@@ -59,6 +60,7 @@ func TestBadPrePrepare(t *testing.T) {
 	}
 	end := view.Start()
 
+	// TODO check with wrong sequence number
 	// prePrepare with wrong view number
 	msg := &protos.Message{
 		Content: &protos.Message_PrePrepare{
@@ -85,6 +87,155 @@ func TestBadPrePrepare(t *testing.T) {
 	fd.AssertCalled(t, "Complain")
 
 	end.Wait()
+
+	// check prePrepare with verifier returning error
+	verifier := &mocks.Verifier{}
+	verifier.On("VerifyProposal", mock.Anything, mock.Anything).Return(errors.New(""))
+	view.Verifier = verifier
+	end = view.Start()
+
+	msg = &protos.Message{
+		Content: &protos.Message_PrePrepare{
+			PrePrepare: &protos.PrePrepare{
+				View:     1,
+				Seq:      0,
+				Proposal: &protos.Proposal{},
+			},
+		},
+	}
+
+	// sent from node who is not the leader, simply ignore
+	view.HandleMessage(2, msg)
+
+	// sent from the leader
+	syncWG.Add(1)
+	fdWG.Add(1)
+	view.HandleMessage(1, msg)
+	syncWG.Wait()
+	fdWG.Wait()
+
+	end.Wait()
+
+}
+
+func TestBadPrepare(t *testing.T) {
+	basicLog, err := zap.NewDevelopment()
+	assert.NoError(t, err)
+	log := basicLog.Sugar()
+	synchronizer := &mocks.Synchronizer{}
+	syncWG := &sync.WaitGroup{}
+	synchronizer.On("SyncIfNeeded", mock.Anything).Run(func(args mock.Arguments) {
+		syncWG.Done()
+	})
+	fd := &mocks.FailureDetector{}
+	fdWG := &sync.WaitGroup{}
+	fd.On("Complain", mock.Anything).Run(func(args mock.Arguments) {
+		fdWG.Done()
+	})
+	comm := &mocks.Comm{}
+	commWG := sync.WaitGroup{}
+	comm.On("Broadcast", mock.Anything).Run(func(args mock.Arguments) {
+		commWG.Done()
+	})
+	verifier := &mocks.Verifier{}
+	verifier.On("VerifyProposal", mock.Anything, mock.Anything).Return(nil)
+	signer := &mocks.Signer{}
+	signer.On("SignProposal", mock.Anything).Return(&types.Signature{
+		Id:    4,
+		Value: []byte{4},
+	})
+	view := &bft.View{
+		Logger:           log,
+		N:                4,
+		LeaderID:         1,
+		Number:           1,
+		ProposalSequence: new(uint64),
+		Sync:             synchronizer,
+		FailureDetector:  fd,
+		Comm:             comm,
+		Verifier:         verifier,
+		Signer:           signer,
+	}
+	end := view.Start()
+
+	pp := &protos.Message{
+		Content: &protos.Message_PrePrepare{
+			PrePrepare: &protos.PrePrepare{
+				View: 1,
+				Seq:  0,
+				Proposal: &protos.Proposal{
+					Header:               []byte{0},
+					Payload:              []byte{1},
+					Metadata:             []byte{2},
+					VerificationSequence: 1,
+				},
+			},
+		},
+	}
+
+	commWG.Add(1)
+	view.HandleMessage(1, pp)
+	commWG.Wait()
+
+	proposal := types.Proposal{
+		Header:               []byte{0},
+		Payload:              []byte{1},
+		Metadata:             []byte{2},
+		VerificationSequence: 1,
+	}
+	digest := proposal.Digest()
+
+	// prepare with wrong view
+	prepare := &protos.Message{
+		Content: &protos.Message_Prepare{
+			Prepare: &protos.Prepare{
+				View:   2,
+				Seq:    0,
+				Digest: digest,
+			},
+		},
+	}
+
+	// sent from the leader
+	syncWG.Add(1)
+	fdWG.Add(1)
+	view.HandleMessage(1, prepare)
+	syncWG.Wait()
+	fdWG.Wait()
+
+	end.Wait()
+
+	end = view.Start()
+
+	commWG.Add(1)
+	view.HandleMessage(1, pp)
+	commWG.Wait()
+
+	wrongProposal := types.Proposal{
+		Header:               []byte{1},
+		Payload:              []byte{2},
+		Metadata:             []byte{3},
+		VerificationSequence: 1,
+	}
+	wrongDigest := wrongProposal.Digest()
+
+	// prepare with wrong digest
+	prepare = &protos.Message{
+		Content: &protos.Message_Prepare{
+			Prepare: &protos.Prepare{
+				View:   1,
+				Seq:    0,
+				Digest: wrongDigest,
+			},
+		},
+	}
+
+	view.HandleMessage(1, prepare)
+	view.HandleMessage(2, prepare)
+
+	view.Abort()
+	end.Wait()
+	signer.AssertNotCalled(t, "SignProposal", mock.Anything)
 }
 
 func TestNormalPath(t *testing.T) {
