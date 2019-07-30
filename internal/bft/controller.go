e94609b37b5ed599a5096b7b946141c9b9d19d22
yacovm <yacovm@il.ibm.com>
2019-07-30 19:43:01 +0300

    Remove Future() in View start and wait for it to end in Abort
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- internal/bft/controller.go
+++ internal/bft/controller.go
@@ -52,7 +52,7 @@ type Future interface {
 
 type Proposer interface {
 	Propose(proposal types.Proposal)
-	Start() Future
+	Start()
 	Abort()
 	GetMetadata() []byte
 	HandleMessage(sender uint64, m *protos.Message)
@@ -90,7 +90,6 @@ type Controller struct {
 	currViewNumber     uint64
 
 	viewChange chan viewInfo
-	viewEnd    Future
 
 	stopChan             chan struct{}
 	decisionChan         chan decision
@@ -203,11 +202,11 @@ func (c *Controller) ProcessMessages(sender uint64, m *protos.Message) {
 	// TODO the msg can be a view change message or a tx req coming from a node after a timeout
 }
 
-func (c *Controller) startView(proposalSequence uint64) Future {
+func (c *Controller) startView(proposalSequence uint64) {
 	// TODO view builder according to metadata returned by sync
 	c.currView = c.ProposerBuilder.NewProposer(c.leaderID(), proposalSequence, c.currViewNumber, c.quorum)
+	c.currView.Start()
 	c.Logger.Debugf("Starting view with number %d", c.currViewNumber)
-	return c.currView.Start()
 }
 
 func (c *Controller) changeView(newViewNumber uint64, newProposalSequence uint64) {
@@ -224,10 +223,8 @@ func (c *Controller) changeView(newViewNumber uint64, newProposalSequence uint64
 	c.Logger.Debugf("Aborting current view with number %d", latestView)
 	c.currView.Abort()
 
-	// Wait for previous view to finish
-	c.viewEnd.Wait()
 	c.setCurrentViewNumber(newViewNumber)
-	c.viewEnd = c.startView(newProposalSequence)
+	c.startView(newProposalSequence)
 
 	// If I'm the leader, I can claim the leader token.
 	if iAm, _ := c.iAmTheLeader(); iAm {
@@ -279,7 +276,6 @@ func (c *Controller) run() {
 	defer func() {
 		c.Logger.Infof("Exiting")
 		c.currView.Abort()
-		c.viewEnd.Wait()
 	}()
 
 	for {
@@ -354,7 +350,7 @@ func (c *Controller) Start(startViewNumber uint64, startProposalSequence uint64)
 	c.viewChange = make(chan viewInfo)
 	c.quorum = c.computeQuorum()
 	c.currViewNumber = startViewNumber
-	c.viewEnd = c.startView(startProposalSequence)
+	c.startView(startProposalSequence)
 	if iAm, _ := c.iAmTheLeader(); iAm {
 		c.acquireLeaderToken()
 	}
