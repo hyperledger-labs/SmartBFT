a563f3f4c541024223b7192b2cd8e369f241259f
yacovm <yacovm@il.ibm.com>
2019-08-14 00:37:23 +0300

    move naive chain request forwarding test to test framework
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- test/basic_test.go
+++ test/basic_test.go
@@ -8,6 +8,8 @@ package test
 import (
 	"testing"
 
+	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -107,4 +109,50 @@ func TestLeaderInPartition(t *testing.T) {
 	data3 := <-n3.Delivered
 	assert.Equal(t, data1, data2)
 	assert.Equal(t, data2, data3)
+}
+
+func TestLeaderForwarding(t *testing.T) {
+	t.Parallel()
+	network := make(Network)
+	defer network.Shutdown()
+
+	n0 := newNode(0, network, t.Name())
+	n1 := newNode(1, network, t.Name())
+	n2 := newNode(2, network, t.Name())
+	n3 := newNode(3, network, t.Name())
+
+	n0.Consensus.Start()
+	n1.Consensus.Start()
+	n2.Consensus.Start()
+	n3.Consensus.Start()
+
+	n1.Submit(Request{ID: "1", ClientID: "alice"})
+	n2.Submit(Request{ID: "2", ClientID: "bob"})
+	n3.Submit(Request{ID: "3", ClientID: "carol"})
+
+	numBatchesCreated := countCommittedBatches(n0)
+
+	committedBatches := make([][]AppRecord, 3)
+	for nodeIndex, n := range []*App{n1, n2, n3} {
+		committedBatches = append(committedBatches, make([]AppRecord, numBatchesCreated))
+		for i := 0; i < numBatchesCreated; i++ {
+			record := <-n.Delivered
+			committedBatches[nodeIndex] = append(committedBatches[nodeIndex], *record)
+		}
+	}
+
+	assert.Equal(t, committedBatches[0], committedBatches[1])
+	assert.Equal(t, committedBatches[0], committedBatches[2])
+}
+
+func countCommittedBatches(n *App) int {
+	var numBatchesCreated int
+	for {
+		select {
+		case <-n.Delivered:
+			numBatchesCreated++
+		case <-time.After(time.Millisecond * 500):
+			return numBatchesCreated
+		}
+	}
 }
