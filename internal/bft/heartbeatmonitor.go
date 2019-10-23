0c1407fcf4ff8d46e84304a19b060a09232b0045
Hagar Meir <hagar.meir@ibm.com>
2019-10-23 11:06:58 +0300

    remove redundant check
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- internal/bft/heartbeatmonitor.go
+++ internal/bft/heartbeatmonitor.go
@@ -192,11 +192,6 @@ func (hm *HeartbeatMonitor) handleHeartBeat(sender uint64, hb *smartbftprotos.He
 		return
 	}
 
-	if !hm.follower {
-		hm.logger.Debugf("Heartbeat monitor is not a follower, ignoring; sender: %d, msg: %v", sender, hb)
-		return
-	}
-
 	hm.logger.Debugf("Received heartbeat from %d, last heartbeat was %v ago", sender, hm.lastTick.Sub(hm.lastHeartbeat))
 	hm.lastHeartbeat = hm.lastTick
 }
