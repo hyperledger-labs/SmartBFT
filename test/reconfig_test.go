ccf6456641232ea089da5f64abbd3ba4d2f0c624
yacovm <yacovm@users.noreply.github.com>
2020-03-05 16:04:06 +0200

    reconfig and view change (#432)
    * reconfig and view change
    
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>
    
    * less requests in test
    
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- test/reconfig_test.go
+++ test/reconfig_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -28,9 +27,7 @@ func TestBasicReconfig(t *testing.T) {
 	}
 	startNodes(nodes, &network)
 
-	for i := 1; i < 5; i++ {
-		nodes[0].Submit(Request{ID: fmt.Sprintf("%d", i), ClientID: "alice"})
-	}
+	nodes[0].Submit(Request{ID: "1", ClientID: "alice"})
 
 	data := make([]*AppRecord, 0)
 	for i := 0; i < numberOfNodes; i++ {
@@ -91,9 +88,7 @@ func TestBasicAddNodes(t *testing.T) {
 	}
 	startNodes(nodes, &network)
 
-	for i := 1; i < 5; i++ {
-		nodes[0].Submit(Request{ID: fmt.Sprintf("%d", i), ClientID: "alice"})
-	}
+	nodes[0].Submit(Request{ID: "1", ClientID: "alice"})
 
 	data1 := make([]*AppRecord, 0)
 	for i := 0; i < numberOfNodes; i++ {
@@ -152,7 +147,7 @@ func TestBasicAddNodes(t *testing.T) {
 
 func TestBasicRemoveNodes(t *testing.T) {
 	// In the beginning there are 7 nodes and the quorum size is 5,
-	// after removing 3 nodes the new quorum size should be 4,
+	// after removing 3 nodes the new quorum size should be 3,
 	// so transactions can be committed after the reconfiguration only if it was successful.
 
 	t.Parallel()
@@ -171,9 +166,7 @@ func TestBasicRemoveNodes(t *testing.T) {
 	}
 	startNodes(nodes, &network)
 
-	for i := 1; i < 5; i++ {
-		nodes[0].Submit(Request{ID: fmt.Sprintf("%d", i), ClientID: "alice"})
-	}
+	nodes[0].Submit(Request{ID: "1", ClientID: "alice"})
 
 	data := make([]*AppRecord, 0)
 	for i := 0; i < numberOfNodes; i++ {
@@ -209,6 +202,75 @@ func TestBasicRemoveNodes(t *testing.T) {
 
 	numberOfNodes = 4
 	nodes[0].Submit(Request{ID: "11", ClientID: "alice"})
+	data = make([]*AppRecord, 0)
+	for i := 0; i < numberOfNodes; i++ {
+		d := <-nodes[i].Delivered
+		data = append(data, d)
+	}
+	for i := 0; i < numberOfNodes-1; i++ {
+		assert.Equal(t, data[i], data[i+1])
+	}
+}
+
+func TestViewChangeAfterReconfig(t *testing.T) {
+	// The initial nodes ids are {2,3,4,5}
+	// After reconfiguration a fifth node is added with id 1
+	// however this node is disconnected, and so a view change is needed
+
+	t.Parallel()
+	network := make(Network)
+	defer network.Shutdown()
+
+	testDir, err := ioutil.TempDir("", t.Name())
+	assert.NoErrorf(t, err, "generate temporary test dir")
+	defer os.RemoveAll(testDir)
+
+	numberOfNodes := 4
+	nodes := make([]*App, 0)
+	for i := 2; i <= numberOfNodes+1; i++ {
+		n := newNode(uint64(i), network, t.Name(), testDir)
+		nodes = append(nodes, n)
+	}
+	startNodes(nodes, &network)
+
+	nodes[0].Submit(Request{ID: "1", ClientID: "alice"})
+
+	data := make([]*AppRecord, 0)
+	for i := 0; i < numberOfNodes; i++ {
+		d := <-nodes[i].Delivered
+		data = append(data, d)
+	}
+	for i := 0; i < numberOfNodes-1; i++ {
+		assert.Equal(t, data[i], data[i+1])
+	}
+
+	newNode := newNode(1, network, t.Name(), testDir)
+	nodes = append(nodes, newNode)
+	startNodes(nodes[4:], &network)
+	nodes[4].Disconnect()
+
+	nodes[0].Submit(Request{
+		ClientID: "reconfig",
+		ID:       "10",
+		Reconfig: Reconfig{
+			InLatestDecision: true,
+			CurrentNodes:     nodesToInt(nodes[0].Node.Nodes()),
+			CurrentConfig:    recconfigToInt(types.Reconfig{CurrentConfig: fastConfig}).CurrentConfig,
+		},
+	})
+
+	data = make([]*AppRecord, 0)
+	for i := 0; i < numberOfNodes; i++ {
+		d := <-nodes[i].Delivered
+		data = append(data, d)
+	}
+	for i := 0; i < numberOfNodes-1; i++ {
+		assert.Equal(t, data[i], data[i+1])
+	}
+
+	for i := 0; i < numberOfNodes; i++ {
+		nodes[i].Submit(Request{ID: "11", ClientID: "alice"})
+	}
 	data = make([]*AppRecord, 0)
 	for i := 0; i < numberOfNodes; i++ {
 		d := <-nodes[i].Delivered
