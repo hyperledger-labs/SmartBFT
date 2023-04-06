330d61815a3ca229146975872d6a00e60034672f
Фёдор Партанский <pfi79@mail.ru>
2023-04-06 11:47:26 +0300

    auto fix files
    Signed-off-by: Фёдор Партанский <pfi79@mail.ru>

--- internal/bft/view.go
+++ internal/bft/view.go
@@ -30,6 +30,7 @@ const (
 )
 
 // State can save and restore the state
+//
 //go:generate mockery -dir . -name State -case underscore -output ./mocks/
 type State interface {
 	// Save saves a message.
